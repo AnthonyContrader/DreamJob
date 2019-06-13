d31167990b369aae62d7505bf8a1fba7ae381baa
IlariaRipa <ilaria.ripa@hotmail.it>
2019-06-13 15:59:15 +0200

    modifiche company controller

--- src/main/java/it/contrader/service/CompanyService.java
+++ src/main/java/it/contrader/service/CompanyService.java
@@ -2,42 +2,43 @@ package it.contrader.service;
 
 import java.util.List;
 
-import it.contrader.converter.ConverterCompany;
-
 import it.contrader.dao.CompanyDAO;
-
-import it.contrader.dto.CompanyDTO;
-
 import it.contrader.model.Company;
-
 
 public class CompanyService {
 
 	private CompanyDAO companyDAO;
-
+	/**
+	 * Costruisce un oggetto di tipo CompanyDAO per poterne usare i metodi
+	 */
 	public CompanyService() {
 		this.companyDAO = new CompanyDAO();
 	}
 
-	public List<Company> getAllCompany() {
+	//chiama il metodo del DAO che ottiene una lista di tutti gli user
+	public List<Company> getAll() {
 		return this.companyDAO.getAllCompany();
 	}
+	
+	//chiama il metodo del DAO che restituisce uno user in base al suo id
+	public Company read(int id) {
+		return this.companyDAO.readCompany(id);
+	}
 
-	public boolean insertCompany(CompanyDTO companyDTO) {
-		return this.companyDAO.insertCompany(ConverterCompany.toEntity(companyDTO));
+	//chiama il metodo del DAO che cancella uno user in base al suo id
+	public boolean delete(int Id) {
+		return companyDAO.deleteCompany(Id);
 	}
 	
-	public CompanyDTO readCompany(int Id) {
-		return ConverterCompany.toDTO(this.companyDAO.readCompany(Id));
+	//chiama il metodo del DAO che inserisce un oggetto User
+	public boolean insert(Company company) {
+		return companyDAO.insertCompany(company);
 	}
-	
-	public boolean updateCompany(CompanyDTO companyDTO) {
-		return this.companyDAO.updateCompany(ConverterCompany.toEntity(companyDTO));
+
+	//chiama il metodo del DAO che modifica un utente
+	public boolean update(Company company) {
+		return companyDAO.updateCompany(company);
 	}
-	
-	public boolean deleteCompany(int Id) {
-		return this.companyDAO.deleteCompany(Id);
-	}
-	
-	
+
+
 }
