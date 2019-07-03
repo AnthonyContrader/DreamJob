282a0c89c639c334a6d151c28868af2f00089677
IlariaRipa <ilaria.ripa@hotmail.it>
2019-07-03 13:02:48 +0200

    modifiche candidato

--- src/app/admin/candidato/candidato.component.ts
+++ src/app/admin/candidato/candidato.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidatoDTO } from 'src/dto/candidatodto';
+import { CandidatoService } from 'src/service/candidato.service';
 
 @Component({
   selector: 'app-candidato',
@@ -6,13 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidato.component.css']
 })
 export class CandidatoComponent implements OnInit {
+  candidato: CandidatoDTO[];
+  candidatotoinsert: CandidatoDTO = new CandidatoDTO();
 
-  constructor() { }
+  constructor(private service: CandidatoService) { }
 
   ngOnInit() {
+    this.getCandidato();
   }
 
+  getCandidato() {
+    this.service.getAll().subscribe(candidato => this.candidato = candidato);
+  }
 
+  delete(candidato: CandidatoDTO) {
+    this.service.delete(candidato.id).subscribe(() => this.getCandidato());
+  }
 
-  
+  update(candidato: CandidatoDTO) {
+    this.service.update(candidato).subscribe(() => this.getCandidato());
+  }
+
 }
